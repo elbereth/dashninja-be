d462caee9f07e59f320848c7743daacfb7622db3
Alexandre Devilliers <elbereth@dashninja.pl>
2016-06-05 17:38:30 +0200

    Nodes now returns KeepRunning parameter

--- index.php
+++ index.php
@@ -965,7 +965,7 @@ $app->get('/nodes', function() use ($app,&$mysqli) {
   }
   else {
     // Retrieve all known nodes for current hub
-    $sql = "SELECT NodeName, NodeTestNet, NodeEnabled, NodeType, VersionPath, VersionRaw, VersionDisplay, VersionHandling, KeepUpToDate FROM cmd_nodes n, cmd_hub_nodes h, cmd_versions v WHERE n.NodeId = h.NodeId AND n.VersionID = v.VersionID AND h.HubId = %d";
+    $sql = "SELECT NodeName, NodeTestNet, NodeEnabled, NodeType, VersionPath, VersionRaw, VersionDisplay, VersionHandling, KeepUpToDate, KeepRunning FROM cmd_nodes n, cmd_hub_nodes h, cmd_versions v WHERE n.NodeId = h.NodeId AND n.VersionID = v.VersionID AND h.HubId = %d";
     if ($request->hasQuery('NodeTestnet')) {
       $sql .= sprintf(" AND NodeTestnet = %d",$request->getQuery('NodeTestnet'));
     }
